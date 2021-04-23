08f54436bb7cc7f1f35c561033fdf9cc0677ab9e
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-04-23 17:30:49 +0430

    Test pages app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .views import HomePageView, AboutPageView
+
+
+class HomePageTests(TestCase):
+
+    def setUp(self):
+        self.client.force_login(get_user_model().objects.get_or_create(username='testuser')[0])
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    # def test_homepage_contains_correct_html(self):
+    #     self.assertContains(self.response, 'TWITTER')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve(reverse('home'))
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
+
+
+class AboutPageTests(TestCase):
+
+    def setUp(self):
+        self.client.force_login(get_user_model().objects.get_or_create(username='testuser')[0])
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Mohammad Khosravi')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve(reverse('about'))
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
