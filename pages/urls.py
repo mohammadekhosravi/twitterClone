29c2c005319b69b79b0978f5617f0f2f13e6d9b6
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-05-05 20:05:16 +0430

    Redirect main url to /home and it's test

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('about/', views.AboutPageView.as_view(), name='about')
+    path('', views.HomeRedirectView.as_view(), name='home_redirect'),
+    path('home/', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class HomeRedirectView(RedirectView):
+    permanent= True
+    pattern_name = 'home'
 
 
 class HomePageView(LoginRequiredMixin, TemplateView):
