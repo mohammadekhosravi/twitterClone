291e004163eb08c298f5c0910f81219b68b89337
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-05-06 06:28:30 +0430

    Just commit some code

---
+++ static/js/base.js
@@ -0,0 +1,24 @@
+function closeAlert(event){
+  let element = event.target;
+  while(element.nodeName !== "BUTTON"){
+    element = element.parentNode;
+  }
+  element.parentNode.parentNode.removeChild(element.parentNode);
+}
+
+function toggleNavbar(collapseID){
+  document.getElementById(collapseID).classList.toggle("hidden");
+  document.getElementById(collapseID).classList.toggle("flex");
+}
+
+function openDropdown(event,dropdownID){
+  let element = event.target;
+  while(element.nodeName !== "BUTTON"){
+          element = element.parentNode;
+  }
+  var popper = Popper.createPopper(element, document.getElementById(dropdownID), {
+          placement: 'bottom-start'
+  });
+  document.getElementById(dropdownID).classList.toggle("hidden");
+  document.getElementById(dropdownID).classList.toggle("block");
+}
