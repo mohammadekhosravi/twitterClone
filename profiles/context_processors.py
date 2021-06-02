26afe3414d45fb1f6aee0e89b0d429625e062b9e
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-06-02 16:12:04 +0430

    Fix follow_suggestion custom context processor about Anonymous user have no following

--- profiles/context_processors.py
+++ profiles/context_processors.py
@@ -5,8 +5,10 @@ from django.contrib.auth import get_user_model
 from django.db.models import Count
 
 def follow_suggestion(request):
-    following_ids = request.user.following.values_list('id', flat=True)
-    suggestion = get_user_model().objects.all().exclude(id__in=following_ids).exclude(username=request.user.username)
-    suggestion = suggestion.annotate(followers_count=Count('followers')).order_by('-followers_count')[:3]
+    if request.user.is_authenticated:
+        following_ids = request.user.following.values_list('id', flat=True)
+        suggestion = get_user_model().objects.all().exclude(id__in=following_ids).exclude(username=request.user.username)
+        suggestion = suggestion.annotate(followers_count=Count('followers')).order_by('-followers_count')[:3]
 
-    return {'suggestion': suggestion}
+        return {'suggestion': suggestion}
+    return {'suggestion': None}
